a4a2668feb8b99417d6aaea72c7cbde2ec3b86f8
codenewbie07 <akshit.chaudhary1011@gmail.com>
2024-07-21 18:48:27 +0530

    Add files via upload

---
+++ traffic light.py
@@ -0,0 +1,10 @@
+print("What is the color of traffic light")
+c= input("Enter color: ")
+if(c =="red"):
+    print("Stop")
+elif(c=="yellow"):
+    print("Start your engines")
+elif(c=="green"):
+    print("Go")
+else:
+    print("Enter a valid color")
